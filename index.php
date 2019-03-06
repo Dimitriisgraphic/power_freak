c247287b843dac489a11d56f214050132cd4cfda
Dimitri GOURICHON <chon74@hotmail.fr>
2019-03-06 17:26:01 +0100

    on avance

--- index.php
+++ index.php
@@ -5,9 +5,70 @@
         <title>Titre</title>
         <meta name="description" content="Description de la page" />
     </head>
-
     <body>
-      Contenu
-    </body>
+      <div class="wrapper">
+        <header class="page--head">
+          header
+        </header>
+        <!-- header -->
 
+          <main class="page--main">
+            <section class="content content_intro">
+              <div class="container container_intro">
+                <aside class="citation">
+                    <h1>L’énergie ne se perd jamais,elle se transmet.</h1>
+                    <span class="author">
+                      Julius Robert von Mayer
+                    </span>
+                    <span class="author_date">
+                     (1814 - 1878)
+                    </span>
+                </aside>
+
+                <aside class="paragraf">
+                  <p class="light">La production et la distribution de Power Freak en Suisse nécessitent évidemment de l'énergie, ce qui a des répercussions sur l'environnement. La société Power Freak est tout à fait consciente de sa responsabilité en matière de protection de l'environnement et s'attache à réaliser constamment des améliorations dans ce domaine.</p>
+                  <p class="bold">Découvrez les efforts déployés par Power Freak pour réduire au maximum son empreinte carbone tout au long du cycle de vie d’une canette. </p>
+                </aside>
+              </div>
+
+
+            </section>
+            <!-- fin de intro -->
+
+            <section class="content content_section1">
+                section1
+            </section>
+            <!-- fin de section1-->
+
+            <section class="content content_section2">
+                section2
+            </section>
+            <!-- fin de section2-->
+
+            <section class="content content_section3">
+                section3
+            </section>
+            <!-- fin de section3-->
+
+            <section class="content content_section4">
+                section4
+            </section>
+            <!-- fin de section4-->
+
+            <section class="content content_section5">
+                section5
+            </section>
+            <!-- fin de section5-->
+
+          </main>
+          <!-- fin de main -->
+
+          <footer class="page--foot">
+              footer
+          </footer>
+          <!-- fin de footer -->
+      </div>
+        <!-- fin de wrapper -->
+
+    </body>
 </html>
