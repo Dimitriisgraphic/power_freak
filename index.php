c0ced53e40d827788537ebf329700196ac9f2f21
Dimitri GOURICHON <chon74@hotmail.fr>
2019-03-07 17:17:05 +0100

    j'avance

--- index.php
+++ index.php
@@ -7,58 +7,73 @@
     </head>
     <body>
       <div class="wrapper">
-        <header class="page--head">
-          header
-        </header>
+        <?php include 'partials/page--head.php'; ?>
         <!-- header -->
-
           <main class="page--main">
             <section class="content content_intro">
-              <div class="container container_intro">
-                <aside class="citation">
-                    <h1>L’énergie ne se perd jamais,elle se transmet.</h1>
-                    <span class="author">
-                      Julius Robert von Mayer
-                    </span>
-                    <span class="author_date">
-                     (1814 - 1878)
-                    </span>
-                </aside>
-
-                <aside class="paragraf">
-                  <p class="light">La production et la distribution de Power Freak en Suisse nécessitent évidemment de l'énergie, ce qui a des répercussions sur l'environnement. La société Power Freak est tout à fait consciente de sa responsabilité en matière de protection de l'environnement et s'attache à réaliser constamment des améliorations dans ce domaine.</p>
-                  <p class="bold">Découvrez les efforts déployés par Power Freak pour réduire au maximum son empreinte carbone tout au long du cycle de vie d’une canette. </p>
-                </aside>
+              <div class="container container_flex">
+                <div class="column column_left">
+                    <div class="block_citation">
+                      <span class="citation citation_big">L’énergie</span><span class="citation">ne se perd jamais,</span><span class="citation">elle se transmet.</span>
+                    </div>
+                    <div class="block_signature">
+                      <span class="author">
+                        Julius Robert von Mayer
+                      </span>
+                      <span class="author author_date">
+                       (1814 - 1878)
+                      </span>
+                    </div>
+                </div>
+                <!-- fin de citation -->
+                <div class="column column_right">
+                  <div class="block_paragraphe">
+                    <h1 class="titre_intro">La politique de power freak</h1>
+                    <p class="light">La production et la distribution de Power Freak en Suisse nécessitent évidemment de l'énergie, ce qui a des répercussions sur l'environnement. La société Power Freak est tout à fait consciente de sa responsabilité en matière de protection de l'environnement et s'attache à réaliser constamment des améliorations dans ce domaine.</p>
+                    <p class="bold">Découvrez les efforts déployés par Power Freak pour réduire au maximum son empreinte carbone tout au long du cycle de vie d’une canette. </p>
+                  </div>
+                </div>
+                <!-- fin de paragraphe -->
               </div>
-
-
             </section>
             <!-- fin de intro -->
 
             <section class="content content_section1">
-                section1
+              <h2 class="section_title">LES CANETTES POWER FREAK</h2>
+              <div class="container container_flex">
+                <div class="column column_left">
+                    <div class="block_visual">
+                      illustration
+                    </div>
+                </div>
+                <!-- fin de visual -->
+                <div class="column column_right">
+                  <div class="block_citation">
+                    <h2 class="titre_intro">Légères et 100 % recyclable</h2>
+                    <p class="light">Power Freak a délibérément choisi d'utiliser des canettes en aluminium. Aujourd’hui, leur poids a diminué significativement comparé aux années précédentes, ce qui permet de préserver de précieuses matières premières. Une fois collectées, elles sont entièrement recyclées, à l'infini, sans aucune perte de qualité.</p>
+                    <p class="bold">Découvrez les efforts déployés par Power Freak pour réduire au maximum son empreinte carbone tout au long du cycle de vie d’une canette.</p>
+                  </div>
+                </div>
+                <!-- fin de paragraphe -->
+              </div>
+              <div class="container container_flex">
+                <div class="column column_left">
+                  <div class="block_citation">
+                    <h2 class="titre_intro">Légères et 100 % recyclable</h2>
+                    <p class="light">Power Freak a délibérément choisi d'utiliser des canettes en aluminium. Aujourd’hui, leur poids a diminué significativement comparé aux années précédentes, ce qui permet de préserver de précieuses matières premières. Une fois collectées, elles sont entièrement recyclées, à l'infini, sans aucune perte de qualité.</p>
+                    <p class="bold">Découvrez les efforts déployés par Power Freak pour réduire au maximum son empreinte carbone tout au long du cycle de vie d’une canette.</p>
+                  </div>
+                </div>
+                <!-- fin de visual -->
+                <div class="column column_right">
+                  <div class="block_visual">
+                    illustration
+                  </div>
+                </div>
+                <!-- fin de paragraphe -->
+              </div>
             </section>
             <!-- fin de section1-->
-
-            <section class="content content_section2">
-                section2
-            </section>
-            <!-- fin de section2-->
-
-            <section class="content content_section3">
-                section3
-            </section>
-            <!-- fin de section3-->
-
-            <section class="content content_section4">
-                section4
-            </section>
-            <!-- fin de section4-->
-
-            <section class="content content_section5">
-                section5
-            </section>
-            <!-- fin de section5-->
 
           </main>
           <!-- fin de main -->
